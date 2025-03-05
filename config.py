933459367fc593ab793c371077cabac3643b5364
Jonah Lee <jonahjacqueslee@gmail.com>
2025-03-04 23:02:25 -0800

    add config with unfilled unit counts

---
+++ config.py
@@ -0,0 +1,66 @@
+residences = {
+    "BC": {
+        "name": "Brock Commons",
+        "units_by_size": { # key: units_by_size/unit, val: number of units
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+        }
+    },
+    "EX": {
+        "name": "Exchange",
+        "units_by_size": {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+        }
+    },
+    "FH": {
+        "name": "Fraser Hall",
+        "units_by_size": {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+        }
+    },
+    "IH": {
+        "name": "Iona House",
+        "units_by_size": {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+        }
+    },
+    "KW": {
+        "name": "Saltwater / (tə šxʷhəleləm̓s tə k̓ʷaƛ̓kʷəʔaʔɬ)",
+        "units_by_size": {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+        }
+    },
+    "MD": {
+        "name": "Marine Drive",
+        "units_by_size": {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+        }
+    },
+    "PC": {
+        "name": "Ponderosa Commons",
+        "units_by_size": {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+        }
+    },
+}
+
