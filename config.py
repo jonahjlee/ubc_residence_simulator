9c742f15b11dca58dad16c23c8ebd62eceec8e7a
Jonah Lee <jonahjacqueslee@gmail.com>
2025-03-04 23:13:48 -0800

    fill unit count

--- config.py
+++ config.py
@@ -1,65 +1,55 @@
+# source: https://vancouver.housing.ubc.ca/residences-rooms/residences/
 residences = {
     "BC": {
         "name": "Brock Commons",
         "units_by_size": { # key: units_by_size/unit, val: number of units
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
+            1: 540+8+6,
+            4: 112,
         }
     },
     "EX": {
         "name": "Exchange",
         "units_by_size": {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
+            1: 71+88+5+77,
+            2: 19+15,
+            4: 84,
         }
     },
     "FH": {
         "name": "Fraser Hall",
         "units_by_size": {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
+            6: 22,
         }
     },
     "IH": {
         "name": "Iona House",
         "units_by_size": {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
+            1: 13+4,
+            2: 3,
         }
     },
     "KW": {
         "name": "Saltwater / (tə šxʷhəleləm̓s tə k̓ʷaƛ̓kʷəʔaʔɬ)",
         "units_by_size": {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
+            1: 505+20+7,
+            4: 54,
         }
     },
     "MD": {
         "name": "Marine Drive",
         "units_by_size": {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
+            1: 274,
+            2: 10,
+            3: 29,
+            4: 307,
         }
     },
     "PC": {
         "name": "Ponderosa Commons",
         "units_by_size": {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
+            1: 615+11,
+            2: 113,
+            4: 76,
         }
     },
 }
